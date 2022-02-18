fade1b241f5ff2a35e10a2a1383174aa55d8d3b9
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-02-18 15:03:56 -0300

    Cumprindo o Segundo Requisito

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <h2>Solar System</h2>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
