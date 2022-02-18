15ec99df19355d2a0eb44bf323df7e1867f93bfc
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-02-18 15:18:33 -0300

    Cumprindo o Terceiro Requisito

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 class SolarSystem extends Component {
   render() {
     return (
-      <div data-testid="solar-system">
-        <h2>Solar System</h2>
-      </div>
+      <div data-testid="solar-system" />
     );
   }
 }
