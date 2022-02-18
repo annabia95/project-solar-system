60bb65915ea48380a4c646156949e8fbd8592296
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-02-18 17:26:45 -0300

    Cumprindo o Sexto Requisito

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,11 +5,7 @@ class Title extends Component {
   render() {
     const { headline } = this.props;
     return (
-      <h2>
-        {' '}
-        {`${headline} : Planetas`}
-        {' '}
-      </h2>);
+      <h2>{headline}</h2>);
   }
 }
 
